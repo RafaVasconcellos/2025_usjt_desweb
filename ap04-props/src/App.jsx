e15c784de6bc50e9c0639ffc3a340d6bffddf368
Rafaella Vasconcellos <rafaellaribeirovasconcellos@gmail.com>
2025-04-14 21:58:36 -0300

    refactor(ap04-react): refatora componente pedido
    - ilustramos o uso do props
    - retomar fazendo o componente de feedback
    - agora o hipo tem uma esposa

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -1,31 +1,56 @@
+import Pedido from "./Pedido"
+
 const App = () => {
   return (
     <div className="container border">
+
       <div className="row">
         <div className="col-12">
           <div>
-            <p>Meu Hippo</p>
-            <i className="fa-solid fa-hippo"></i>
+            <p>Meus Hippos</p>
+            <i className="fa-solid fa-hippo fa-3x"></i>
+            <i className="fa-solid fa-hippo fa-3x fa-flip-horizontal"></i>
           </div>
         </div>
       </div>
 
       <div className="row">
         <div className="col-sm-12 col-lg-3 col-md-6">
-          <p className="border border-dark">pedido 1</p>
+          <Pedido 
+            data="22/04/2024"
+            titulo="Café quente"
+            descricao="cafzinho"
+            icone="mug-hot"
+          />
         </div>
 
         <div className="col-sm-12 col-lg-3 col-md-6">
-          <p className="border border-dark">pedido 2</p>
+          <Pedido
+            data="14/04/2025"
+            titulo="Cookie"
+            descricao="Cookie Suculento"
+            icone="cookie"
+          />
         </div>
 
         <div className="col-sm-12 col-lg-3 col-md-6">
-          <p className="border border-dark">pedido 3</p>
+          <Pedido
+            data="14/01/2025"
+            titulo="Mobi"
+            descricao="Carro pequeno"
+            icone="car"
+          />
         </div>
 
         <div className="col-sm-12 col-lg-3 col-md-6">
-          <p className="border border-dark">pedido 4</p>
+          <Pedido
+            data="14/01/2025"
+            titulo="Bicicleta"
+            descricao="Bicicletinha"
+            icone="bicycle"
+          />
         </div>
+
       </div>
     </div>
   )
