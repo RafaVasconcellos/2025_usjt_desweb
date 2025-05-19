c1fdc793ecfe968a741029fd31fd4b582dda7c98
Rafaella Vasconcellos <rafaellaribeirovasconcellos@gmail.com>
2025-05-19 19:17:10 -0300

    feat(ap07-react): adicionado Pexels e .env

--- ap07-fotos/src/App.jsx
+++ ap07-fotos/src/App.jsx
@@ -1,20 +1,45 @@
-
 import { Component } from 'react'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 import 'primereact/resources/themes/bootstrap4-light-purple/theme.css'
+import { createClient } from 'pexels'
 import Busca from './components/Busca'
-
+import ListaImagens from './components/ListaImagens'
+import PexelsLogo from './components/PexelsLogo'
 class App extends Component {
+
+  state = { pics: [] }
+
+  pexelsClient = null
+  pexelsKey = import.meta.env.VITE_PEXELS_KEY
+
+  componentDidMount() {
+    this.pexelsClient = createClient(
+      this.pexelsKey
+    )
+  }
+
+  onBuscaRealizada = (termo) => {
+    this.pexelsClient.photos.search({ query: termo })
+      .then((result) => this.setState({ pics: result.photos }))
+  }
+
   render() {
     return (
-      <div className="grid justify-content-center m-auto w-9 border-round border-1 border-400">
-        <div className="col-12">
+      <div className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
+        <div className='col-12'>
+          <div className='col-12'>
+            <PexelsLogo />
+          </div>
           <h1>Exibir uma lista de...</h1>
         </div>
-        <div className="col-8">
-          <Busca />
+        <div className='col-8'>
+          <Busca
+            onBuscaRealizada={this.onBuscaRealizada} />
+        </div>
+        <div className='col-8'>
+          <ListaImagens pics={this.state.pics} />
         </div>
       </div>
     )
--- ap07-fotos/src/components/Busca.jsx
+++ ap07-fotos/src/components/Busca.jsx
@@ -1,36 +1,43 @@
-import { Component } from 'react';
+import { Component } from 'react'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
 import { Button } from 'primereact/button'
 
 export default class Busca extends Component {
-  state = {
-    termoDeBusca: ''
-  }
-  onTermoAlterado = (event) => {
-    console.log(event.target.value)
-  }
-  render() {
-    return (
-      //empilhando os filhos
-      <div className="flex flex-column">
-        {/*ícone à esquerda, largura máxima*/}
-        <span className="p-input-icon-left w-full">
-          <i className="pi pi-search" />
-          <InputText
-            className="w-full"
-            onChange={this.onTermoAlterado}
-            placeholder={this.props.dica}
-          />
-        </span>
-        <Button
-          label='OK'
-          className="p-button-outlined mt-2"
-        />
-      </div>
-    )
-  }
+	state = {
+		termoDeBusca: ''
+	}
+	onTermoAlterado = (event) => {
+		this.setState({ termoDeBusca: event.target.value })
+	}
+	onFormSubmit = (event) => {
+		event.preventDefault()
+		this.props.onBuscaRealizada(this.state.termoDeBusca)
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.onFormSubmit}>
+				<div className="flex flex-column">
+					<IconField iconPosition="left">
+						<InputIcon className="pi pi-search"></InputIcon>
+						<InputText
+							className="w-full"
+							value={this.state.termoDeBusca}
+							onChange={this.onTermoAlterado}
+							placeholder="O que quer ver?" />
+					</IconField>
+					<Button
+						label='OK'
+						className="p-button-outlined mt-2"
+					/>
+				</div>
+			</form>
+		)
+	}
 }
 
 Busca.defaultProps = {
-  dica: '    Digite algo que deseja ver...'
+	dica: 'Digite algo que deseja ver...',
 }
