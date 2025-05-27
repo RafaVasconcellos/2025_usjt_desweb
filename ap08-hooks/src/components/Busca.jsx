92b50605ba1a8d67b7853263f44d9619eff89188
Rafaella Vasconcellos <rafaellaribeirovasconcellos@gmail.com>
2025-05-26 21:53:03 -0300

    feat(ap08-hooks): encerra apostila
    - adiciona axios
    - altera o componente de Busca
    - cria lista a partir da busca

--- ap08-hooks/src/components/Busca.jsx
+++ ap08-hooks/src/components/Busca.jsx
@@ -1,14 +1,64 @@
+import axios from 'axios';
+import striptags from 'striptags';
+import { useEffect, useState } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
 
 const Busca = () => {
+  const [ termoDeBusca, setTermoDeBusca ] = useState('React');
+  const [ resultados, setResultados ] = useState([]);
+
+  console.log(resultados)
+
+  useEffect(() => {
+    const fazBusca = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          format: 'json',
+          srsearch: termoDeBusca,
+          origin: '*'
+        }
+      });
+      setResultados(data.query.search)
+    }
+
+    // if(termoDeBusca)
+      fazBusca();
+  }, [termoDeBusca]);
+
   return (
     <div>
       <IconField iconPosition="left">
         <InputIcon className="pi pi-search" />
-        <InputText />
+        <InputText
+          placeholder='Buscar...'
+          value={termoDeBusca}
+          onChange={(event) => {
+            setTermoDeBusca(event.target.value)
+          }}
+        />
       </IconField>
+      {
+        resultados.map((resultado) => (
+          <div 
+            key={resultado.pageid}
+            className='my-3 border-1 border-400'
+          >
+            <div className='border-bottom border-1 border-400 p-3 text-center font-bold'>
+              {resultado.title}
+            </div>
+            <div>
+              <span dangerouslySetInnerHTML={{__html: resultado.snippet}}>
+
+              </span>
+              {striptags(resultado.snippet)}
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
